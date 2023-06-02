cf8308583378d4f4a2cb32f13c384f2d42bcb9f7
Joji Mitsumoto <mitsumotocs@gmail.com>
2023-06-02 10:48:52 +0900

    Saved

--- app-1/main.js
+++ app-1/main.js
@@ -1,4 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Hello, world!');
-    console.log(Numpad.KEY_1);
+import { Numpad } from "../libs/Numpad.js";
+
+const textareaElement = document.querySelector('textarea');
+console.log(textareaElement);
+
+textareaElement.addEventListener('input', (event) => {
+    const text = event.target.value;
+    //console.log(text);
 });
