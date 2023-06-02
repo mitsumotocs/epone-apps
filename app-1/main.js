2bc98736b9605a795d48b47dc2a6e8b2f9da2f78
Joji Mitsumoto <mitsumotocs@gmail.com>
2023-06-02 15:02:40 +0900

    Saved

--- app-1/main.js
+++ app-1/main.js
@@ -1,9 +1,18 @@
 import { Numpad } from "../libs/Numpad.js";
 
 const textareaElement = document.querySelector('textarea');
-console.log(textareaElement);
-
-textareaElement.addEventListener('input', (event) => {
+textareaElement.addEventListener('keydown', (event) => {
     const text = event.target.value;
-    //console.log(text);
+    switch (event.key) {
+        case Numpad.KEY_STAR:
+            event.preventDefault();
+            event.target.value = text.substring(0, text.length - 1);
+            break;
+        case Numpad.KEY_SHARP:
+            event.preventDefault();
+            event.target.value = '';
+            break;
+        default:
+            break;
+    }
 });
